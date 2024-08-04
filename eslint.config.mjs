d9bfca13969e4ab743a3eb472f05aebbacc6c0ec
ViktorBykh <129959009+ViktorBykh@users.noreply.github.com>
2024-08-04 16:11:45 +0300

    feat: creating `ESlint` config

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+export default antfu(
+  ...compat.config({ extends: ['plugin:tailwindcss/recommended'] }),
+  {
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
+  },
+)
