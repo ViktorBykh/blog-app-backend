8d7229cabf76c12115d4e3e3db2d9766c841385e
ViktorBykh <129959009+ViktorBykh@users.noreply.github.com>
2024-08-04 16:17:38 +0300

    feat: creating `Post` route

---
+++ src/routes/postRoute.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {
+  createPost,
+  deletePost,
+  getPostById,
+  getPosts,
+  updatePost,
+} from '../controllers/postController.js'
+
+const router = express.Router()
+
+router.get('/', getPosts)
+
+router.get('/:id', getPostById)
+
+router.post('/', createPost)
+
+router.put('/:id', updatePost)
+
+router.delete('/:id', deletePost)
+
+export default router
