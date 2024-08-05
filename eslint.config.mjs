7e406767f6830197c0543180735a57f8bfa36b21
ViktorBykh <129959009+ViktorBykh@users.noreply.github.com>
2024-08-05 10:33:58 +0300

    feat: adding env `mocha` to `ESlint` config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,10 @@ import { FlatCompat } from '@eslint/eslintrc'
 const compat = new FlatCompat()
 
 export default antfu(
-  ...compat.config({ extends: ['plugin:tailwindcss/recommended'] }),
+  ...compat.config({
+    extends: ['plugin:tailwindcss/recommended'],
+    env: { mocha: true },
+  }),
   {
     rules: {
       'node/prefer-global/process': 'off',
